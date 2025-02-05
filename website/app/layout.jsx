c0ece2c14d3f9a941282b91239064d899e75c9d7
Jon Couldridge <jcouldridge@outlook.com>
2025-02-05 15:05:56 +0000

    reorganising; synthetic data

--- website/app/layout.jsx
+++ website/app/layout.jsx
@@ -1,22 +1,21 @@
-import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import { Banner, Head } from 'nextra/components'
-import { getPageMap } from 'nextra/page-map'
-import 'nextra-theme-docs/style.css'
- 
+import { Footer, Layout, Navbar } from "nextra-theme-docs";
+import { Banner, Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import "nextra-theme-docs/style.css";
+
 export const metadata = {
   // Define your metadata here
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
-}
- 
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
+};
+
 const navbar = (
   <Navbar
-    logo={<b>Nextra</b>}
+    logo={<b>Bacalhau Hackathon</b>}
     // ... Your additional navbar options
   />
-)
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
- 
+);
+const footer = <Footer>MIT {new Date().getFullYear()} © University of Nottingham.</Footer>;
+
 export default async function RootLayout({ children }) {
   return (
     <html
@@ -34,10 +33,9 @@ export default async function RootLayout({ children }) {
       </Head>
       <body>
         <Layout
-          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/Health-Informatics-UoN/bacalhau-hack/tree/main"
           footer={footer}
           // ... Your additional layout options
         >
@@ -45,5 +43,5 @@ export default async function RootLayout({ children }) {
         </Layout>
       </body>
     </html>
-  )
+  );
 }
